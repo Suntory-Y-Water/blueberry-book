b46ce40e9b7dba834224128c38a48a91409b13e0
sui <1202ayasnppk@gmail.com>
2023-08-12 14:55:53 +0900

    3.6

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,24 @@
-// https://typescriptbook.jp/reference/values-types-variables/discriminated-union#%E5%88%A4%E5%88%A5%E5%8F%AF%E8%83%BD%E3%81%AA%E3%83%A6%E3%83%8B%E3%82%AA%E3%83%B3%E5%9E%8B%E3%81%AE%E7%B5%9E%E3%82%8A%E8%BE%BC%E3%81%BF
-interface User {
+// 配列の分割代入　何回もarr[0]みたいに書く必要がなくなる
+const arr = [2, 3, 5, 7, 11]
+const [first, second, third] = arr
+// console.log(first);
+
+type Employee = {
+  id: number;
   name: string;
   age: number;
-  occupation: string;
-}
+  position: string;
+};
 
-interface Admin {
-  name: string;
-  age: number;
-  role: string;
-}
+const employee: Employee = {
+  id: 1,
+  name: 'John',
+  age: 30,
+  position: 'Engineer',
+};
 
-export type Person = User | Admin;
+const { id, age, ...rest } = employee;
 
-export const persons: Person[] = [
-  {
-    name: "Max Mustermann",
-    age: 25,
-    occupation: "Chimney sweep",
-  },
-  {
-    name: "Jane Doe",
-    age: 32,
-    role: "Administrator",
-  },
-  {
-    name: "Kate Müller",
-    age: 23,
-    occupation: "Astronaut",
-  },
-  {
-    name: "Bruce Willis",
-    age: 64,
-    role: "World saver",
-  },
-];
-
-export function logPerson(person: Person) {
-  let additionalInformation: string;
-  if ('role' in person) {
-    additionalInformation = person.role;
-    console.log(person.role);
-  } else {
-    additionalInformation = person.occupation;
-    console.log(person.occupation);
-  }
-  console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
-}
-
-persons.forEach(logPerson);
+console.log(id); // 1
+console.log(age);
+console.log(rest); // { name: 'John', age: 30, position: 'Engineer' }
