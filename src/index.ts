09a17887b99eaaa4b32d2237604bf8b073f2e03c
sui <1202ayasnppk@gmail.com>
2023-09-11 07:26:27 +0900

    8.3.4

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,21 @@
-class ForeignUser {
-  // 修飾子をつけないとただの引数宣言になってしまう
-  // TypeScript特有らしい
-  // constructor(public name: string, private age:number, readonly locate: string){}
+import { readFile} from 'fs/promises'
 
-  name: string;
-  private age: number
+const p = readFile('./src/bk/foo.txt', 'utf-8')
 
-  constructor(name: string, age: number){
-    this.name = name;
-    this.age = age
-  }
+// errorにはunknownをつける
+p.then((result) => {
+  console.log('成功',result);
+},(error: unknown) => {
+  console.log('失敗', error);
+})
 
-  isAdult(): boolean {
-    return this.age >= 20
-  }
+// 8.3.4 自分でPromiseを定義してみよう
+const countTime = new Promise<number>((resolve) => {
+  setTimeout(() => {
+    resolve(100);
+  }, 2000);
+})
 
-  setAge(newAge: number){
-    return this.age = newAge
-  }
-}
-
-const foreignUser = new ForeignUser("john", 24)
-console.log(foreignUser.setAge(18));
-console.log(foreignUser.isAdult());
-
-class Animal {
-  public name: string;
-
-  public constructor(theName: string) {
-    this.name = theName;
-  }
-
-  protected move(distanceInMeters: number) {
-    console.log(`${this.name} moved ${distanceInMeters}m.`);
-  }
-}
-class Gorilla extends Animal {
-  move(distanceInMeters: number) {
-    super.move(distanceInMeters * 10);
-  }
-}
-
-const gorilla = new Gorilla("速いゴリラ");
-gorilla.move(10);
+countTime.then((num) => {
+  console.log(`結果は${num}`);
+})
